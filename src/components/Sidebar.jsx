9f79eea26af76ce8ef2b104ad485df113b6fefda
Dameli <dameli.ryspay@gmail.com>
2021-08-29 00:50:35 +0600

    add icons to sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,19 +2,33 @@ import React from "react";
 // import { useState, useEffect } from "react";
 // import { Link } from "react-router-dom";
 import "./Sidebar.css";
+import { AiOutlineHome } from "react-icons/ai";
+import { BsPersonFill } from "react-icons/bs";
+import { IoMdNotifications } from "react-icons/io";
+import { MdEmail, MdLanguage } from "react-icons/md";
+import { FiSettings } from "react-icons/fi";
+
 // import { Button } from "./Button";
 
 function Sidebar() {
   return (
     <>
       <nav className="sidebar">
-        <span className="icon home" />
-        <span className="icon profile" />
-        <span className="icon course" />
-        <span className="icon microphone" />
-        <span className="icon games" />
-        <span className="icon videos" />
-        <span className="icon exercises" />
+        <ul className="content-links">
+          <AiOutlineHome className="icon home" />
+          <BsPersonFill className="icon profile" />
+          <span className="icon course" />
+          <span className="icon podcasts" />
+          <span className="icon games" />
+          <span className="icon videos" />
+          <span className="icon exercises" />
+        </ul>
+        <ul className="settings-links">
+          <IoMdNotifications className="icon notifications" />
+          <MdEmail className="icon email" />
+          <MdLanguage className="icon language" />
+          <FiSettings className="icon settings" />
+        </ul>
       </nav>
     </>
   );
